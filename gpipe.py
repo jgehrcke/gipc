63429c4cc8eabdb41482def706903a292f85b6ba
Jan-Philip Gehrcke <Jan-Philip Gehrcke@localhost>
2012-11-07 23:11:11 +0100

    Add TODO entry.

--- gpipe.py
+++ gpipe.py
@@ -14,6 +14,9 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+"""
+TODO: rename post/pre_fork, as fork is not from windows world.
+"""
 
 import os
 import sys
