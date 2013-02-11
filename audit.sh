d9fb21933c9ddc1e41a264b977b678e9c8594fb3
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-02-11 10:49:32 +0100

    Update audit and docs regarding code coverage.

--- audit.sh
+++ audit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo "Running gipc unit tests..."
-cd test
-py.test
-cd ..
+echo "Running gipc unit tests, investigate code coverage..."
+py.test --cov gipc test
+if [ -d coverage_html ]; then
+    rm -rf coverage_html/*
+fi
+coverage html -d coverage_html
 echo -e "\n\nRunning setup.py check & readme2html..."
 python setup.py check
 python setup.py --long-description | rst2html.py > /dev/null
 echo -e "\n\nRunning PEP8 check..."
 pep8
 echo -e "\n\nRunning pylint..."
-cd gipc
-pylint --reports=n --include-ids=y --disable=C0103,W0212,W0511,W0142,R0903 gipc.py
-cd ..
+pylint --reports=n --include-ids=y --disable=C0103,W0212,W0511,W0142,R0903 gipc/gipc.py
