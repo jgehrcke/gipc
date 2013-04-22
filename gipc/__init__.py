a89dd3ae145902ed4602998e89b8aa9b180a06d2
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-04-22 10:40:01 +0200

    Fix issue #1: don't import gipc in setup.py, read version number from __init__.py via regex.

--- gipc/__init__.py
+++ gipc/__init__.py
@@ -2,8 +2,7 @@
 # Copyright 2012-2013 Jan-Philip Gehrcke. See LICENSE file for details.
 
 
-version_info = (0, 3, 0)
-__version__ = ".".join(map(str, version_info))
+__version__ = '0.3.0'
 
 
 from gipc import pipe, start_process, GIPCError, GIPCClosed, GIPCLocked
