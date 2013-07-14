5a525957e8ae8079bcf81b3eac192484117b8ade
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-07-14 17:15:42 +0200

    Prepare 0.3.2 release.

--- gipc/__init__.py
+++ gipc/__init__.py
@@ -2,7 +2,7 @@
 # Copyright 2012-2013 Jan-Philip Gehrcke. See LICENSE file for details.
 
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 
 from gipc import pipe, start_process, GIPCError, GIPCClosed, GIPCLocked
