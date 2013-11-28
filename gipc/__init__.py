9e609562ef23470983ea333f9ea47a123cc2566a
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-11-28 16:14:09 +0100

    Bump version to 0.4.0 in __init__.py.

--- gipc/__init__.py
+++ gipc/__init__.py
@@ -2,7 +2,7 @@
 # Copyright 2012-2013 Jan-Philip Gehrcke. See LICENSE file for details.
 
 
-__version__ = '0.3.2'
+__version__ = '0.4.0'
 
 
 from gipc import pipe, start_process, GIPCError, GIPCClosed, GIPCLocked
