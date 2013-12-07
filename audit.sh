1fb14a4015d25216b7b21e4af17675400b6a26d1
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-12-07 18:49:45 +0100

    Fix audit.sh (new pylint version, improper placement of cd ..).

--- audit.sh
+++ audit.sh
@@ -6,8 +6,8 @@ if [ $? -eq 0 ]; then
         rm -rf coverage_html/*
     fi
     coverage html -d coverage_html
-    cd ..
 fi
+cd ..
 echo -e "\n>>> Running setup.py check..."
 python setup.py check
 echo -e "\n>>> Running python setup.py --long-description | rst2html.py > /dev/null..."
@@ -17,4 +17,4 @@ rst2html.py CHANGELOG.rst > /dev/null
 echo -e "\n>>> Running PEP8 check..."
 pep8
 echo -e "\n>>> Running pylint..."
-pylint --reports=n --include-ids=y --disable=C0103,W0212,W0511,W0142,R0903 gipc/gipc.py
+pylint --reports=n --disable=C0103,W0212,W0511,W0142,R0903 gipc/gipc.py
