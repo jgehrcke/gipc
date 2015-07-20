9d2004ba367591dd65d2afb9a3c073d33a8731f9
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2015-07-20 21:37:59 +0200

    __init__.py: update version number to 0.6.0, update copyright notice.

--- gipc/__init__.py
+++ gipc/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# Copyright 2012-2014 Jan-Philip Gehrcke. See LICENSE file for details.
+# Copyright 2012-2015 Jan-Philip Gehrcke. See LICENSE file for details.
 
 
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 
 
 from .gipc import pipe, start_process, GIPCError, GIPCClosed, GIPCLocked
