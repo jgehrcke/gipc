75a8ec0162389d5709f1fcd2682712fbbeefd57a
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2015-07-20 21:32:47 +0200

    audit.sh: simplify py.test w/ coverage call signature.

--- audit.sh
+++ audit.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 echo ">>> Running gipc unit tests, investigate code coverage..."
-cd test && py.test --cov gipc
-if [ $? -eq 0 ]; then
-    if [ -d coverage_html ]; then
-        rm -rf coverage_html/*
-    fi
-    coverage html -d coverage_html
-fi
+cd test && py.test --cov-report term --cov-report html --cov gipc
 cd ..
 echo -e "\n>>> Running setup.py check..."
 python setup.py check
