38b203fc40030001442dc94d6891a36bddc4a237
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2015-07-22 13:51:33 +0200

    Cosmetic changes to audit.sh.

--- audit.sh
+++ audit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo ">>> Running gipc unit tests, investigate code coverage..."
+echo ">>> Run gipc unit tests, investigate code coverage ..."
 cd test && py.test --cov-report term --cov-report html --cov gipc
 cd ..
-echo -e "\n>>> Running setup.py check..."
+echo -e "\n>>> Run setup.py check..."
 python setup.py check
-echo -e "\n>>> Running python setup.py --long-description | rst2html.py > /dev/null..."
+echo -e "\n>>> Run python setup.py --long-description | rst2html.py > /dev/null ..."
 python setup.py --long-description | rst2html.py > /dev/null
-echo -e "\n>>> Running rst2html.py CHANGELOG.rst > /dev/null..."
+echo -e "\n>>> Run rst2html.py CHANGELOG.rst > /dev/null ..."
 rst2html.py CHANGELOG.rst > /dev/null
-echo -e "\n>>> Running PEP8 check..."
+echo -e "\n>>> Run PEP8 check ..."
 pep8
-echo -e "\n>>> Running pylint..."
+echo -e "\n>>> Run pylint on gipc/gipc.py ..."
 pylint --reports=n --disable=C0103,W0212,W0511,W0142,R0903 gipc/gipc.py
