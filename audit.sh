e6706b9465edc90d1eb4a7ff55a9fd23c46d7de1
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2017-11-08 23:19:04 +0100

    Update audit.sh, run as part of CI

--- audit.sh
+++ audit.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-echo ">>> Run gipc unit tests, investigate code coverage ..."
-cd test && py.test --cov-report term --cov-report html --cov gipc
-cd ..
-echo -e "\n>>> Run setup.py check..."
+set -ex
 python setup.py check
-echo -e "\n>>> Run python setup.py --long-description | rst2html.py > /dev/null ..."
 python setup.py --long-description | rst2html.py > /dev/null
-echo -e "\n>>> Run rst2html.py CHANGELOG.rst > /dev/null ..."
 rst2html.py CHANGELOG.rst > /dev/null
-echo -e "\n>>> Run PEP8 check ..."
-pep8
-echo -e "\n>>> Run pylint on gipc/gipc.py ..."
-pylint --reports=n --disable=C0103,W0212,W0511,W0142,R0903 gipc/gipc.py
+
+# Run flake8 on the gipc directory (do not yet
+# run on examples and test code).
+flake8 gipc/
+
+# The pylint result is not to be interpreted in a binary fashion.
+# pylint --reports=n --disable=C0103,W0212,W0511,W0142,R0903 gipc/gipc.py
