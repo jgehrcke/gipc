78e710f232e28286f4ebc2db0490279f1f742ca4
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2017-11-11 20:09:56 +0100

    Change version to 0.7.0-dev

--- gipc/__init__.py
+++ gipc/__init__.py
@@ -3,7 +3,7 @@
 # Copyright 2012-2017 Jan-Philip Gehrcke. See LICENSE file for details.
 
 
-__version__ = '0.6.0'
+__version__ = '0.7.0-dev'
 
 
 from .gipc import pipe, start_process, GIPCError, GIPCClosed, GIPCLocked
