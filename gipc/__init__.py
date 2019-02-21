d8fcd5bce92830648e27ef714bf5931d7262dc7f
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2019-02-21 16:55:46 +0100

    Bump version number to 1.0.1

--- gipc/__init__.py
+++ gipc/__init__.py
@@ -3,7 +3,7 @@
 # Copyright 2012-2018 Dr. Jan-Philip Gehrcke. See LICENSE file for details.
 
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 
 from .gipc import pipe, start_process, GIPCError, GIPCClosed, GIPCLocked
