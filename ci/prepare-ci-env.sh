5762684898e454bce038b7e7cf8e0e701cfb89f9
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2020-09-14 16:03:00 +0200

    ci: clean up prepare-ci-env.sh

--- ci/prepare-ci-env.sh
+++ ci/prepare-ci-env.sh
@@ -8,23 +8,11 @@ set -o xtrace
 if [[ -z "${PYENV_PYTHON_VERSION}" ]]; then
     echo "Use Travis-provided Python"
 else
-    # Travis seems to set PYENV_ROOT to /opt/pyenv which holds an old pyenv
-    # release. Use a more recent release. Note that PYENV_RELEASE might
-    # misleadingly be set from the outside if we don't set it here.
-    export PYENV_RELEASE="v1.2.16"
     export PYENV_ROOT="$HOME/.travis-pyenv"
-
-    # Note(JP): based on praekeltfoundation/travis-pyenv/blob/develop/setup-pyenv.sh
-    # but w/o caching. See https://github.com/jgehrcke/gipc/issues/92.
     mkdir "$PYENV_ROOT"
-    #curl -fsSL --retry 10 "https://github.com/pyenv/pyenv/archive/$PYENV_RELEASE.tar.gz" \
-    #    | tar -xz -C "$PYENV_ROOT" --strip-components 1
-
-    # Use head of master again.
+    # Use head of master of pyenv
     git clone https://github.com/pyenv/pyenv "$PYENV_ROOT"
-
     export PATH="$PYENV_ROOT/bin:$PATH"
-
     export PYTHON_BUILD_CURL_OPTS="--retry 10 --connect-timeout 10 --max-time 60"
 
     pyenv install -v "$PYENV_PYTHON_VERSION"
