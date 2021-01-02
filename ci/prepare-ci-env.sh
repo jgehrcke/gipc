8921da9407ab4c5ba2fdbd4150a782ca06d7b57c
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2021-01-02 21:18:08 +0100

    ci: bump pip/setuptools versions

--- ci/prepare-ci-env.sh
+++ ci/prepare-ci-env.sh
@@ -62,8 +62,8 @@ fi
 # Install newer pip and setuptools (newer than bundles with certain Python
 # releases and newer than what Travis brings) -- but still pin the versions so
 # that there are no moving dependencies.
-pip install 'pip==19.1.1' --upgrade
-pip install 'setuptools==42.0.2' --upgrade
+pip install 'pip==20.3.3' --upgrade
+pip install 'setuptools==51.1.1' --upgrade
 
 # Install gipc dependencies from its `setup.py`.
 pip install .
