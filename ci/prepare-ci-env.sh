d9730ad8efbfee310b3a6698f4a337b3a4b066ef
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2022-01-13 13:11:45 +0100

    ci: setuptools 59.6.0
    60.5.0 seems to be too new.

--- ci/prepare-ci-env.sh
+++ ci/prepare-ci-env.sh
@@ -8,7 +8,7 @@ set -o xtrace
 # releases and newer than what Travis brings) -- but still pin the versions so
 # that there are no moving dependencies.
 pip install 'pip==21.3.1' --upgrade
-pip install 'setuptools==60.5.0' --upgrade
+pip install 'setuptools==59.6.0' --upgrade
 
 # Install gipc dependencies from its `setup.py`. Also: "DEPRECATION: A future
 # pip version will change local packages to be built in-place without first
