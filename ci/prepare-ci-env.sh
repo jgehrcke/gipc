371526415dfe69ca7c989ac926f9e8113c372f0e
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2024-11-08 09:24:47 +0100

    wip

--- ci/prepare-ci-env.sh
+++ ci/prepare-ci-env.sh
@@ -7,8 +7,8 @@ set -o xtrace
 # Install newer pip and setuptools (newer than bundles with certain Python
 # releases and newer than what Travis brings) -- but still pin the versions so
 # that there are no moving dependencies.
-pip install 'pip==23.3.2' --upgrade
-pip install 'setuptools==69.0.2' --upgrade
+pip install 'pip==24.3.1' --upgrade
+pip install 'setuptools==75.3.0' --upgrade
 
 pip install .
 
